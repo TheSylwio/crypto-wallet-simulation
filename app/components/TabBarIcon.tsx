01df63c5eb4cc821252faf103df96087b867d404
thesylwio <thesylwio@gmail.com>
2021-03-26 22:44:09 +0100

    🚚 Moved TapBarIcon to its own component

---
+++ app/components/TabBarIcon.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Ionicons } from '@expo/vector-icons';
+
+const TabBarIcon = (props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string }) => {
+  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
+};
+
+export default TabBarIcon;

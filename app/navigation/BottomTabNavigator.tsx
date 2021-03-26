dc790d8f6824605d5d73e718fa3161b6ac3874cb
thesylwio <thesylwio@gmail.com>
2021-03-26 22:40:19 +0100

    🚚 Refactored BottomTabNavigator

--- app/navigation/BottomTabNavigator.tsx
+++ app/navigation/BottomTabNavigator.tsx
@@ -1,70 +1,38 @@
-import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
-
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
-import WalletScreen from '../screens/WalletScreen';
-import SettingsScreen from '../screens/SettingsScreen';
-import { BottomTabParamList, WalletParamList, SettingsParamList } from '../../types';
+import HomeNavigator from './HomeNavigator';
+import WalletNavigator from './WalletNavigator';
+import { BottomTabParamList } from '../../types';
+import TabBarIcon from '../components/TabBarIcon';
 
 const BottomTab = createBottomTabNavigator<BottomTabParamList>();
-const TabBarIcon = (props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string }) => {
-  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
-}
-
-const WalletStack = createStackNavigator<WalletParamList>();
-
-const WalletNavigator = () => {
-  return (
-    <WalletStack.Navigator>
-      <WalletStack.Screen
-        name="WalletScreen"
-        component={WalletScreen}
-        options={{ headerTitle: 'Wallet' }}
-      />
-    </WalletStack.Navigator>
-  );
-}
-
-const SettingsStack = createStackNavigator<SettingsParamList>();
-
-const SettingsNavigator = () => {
-  return (
-    <SettingsStack.Navigator>
-      <SettingsStack.Screen
-        name="SettingsScreen"
-        component={SettingsScreen}
-        options={{ headerTitle: 'Settings' }}
-      />
-    </SettingsStack.Navigator>
-  );
-}
 
 const BottomTabNavigator = () => {
   const colorScheme = useColorScheme();
 
   return (
     <BottomTab.Navigator
-      initialRouteName="Wallet"
-      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}>
+      initialRouteName="Home"
+      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}
+    >
+      <BottomTab.Screen
+        name="Home"
+        component={HomeNavigator}
+        options={{
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color}/>,
+        }}
+      />
       <BottomTab.Screen
         name="Wallet"
         component={WalletNavigator}
         options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="wallet" color={color} />,
-        }}
-      />
-      <BottomTab.Screen
-        name="Settings"
-        component={SettingsNavigator}
-        options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="settings" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="wallet" color={color}/>,
         }}
       />
     </BottomTab.Navigator>
   );
-}
+};
 
 export default BottomTabNavigator;

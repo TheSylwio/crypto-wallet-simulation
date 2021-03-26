22654447ba168ef089182c3331b447daed396e4f
thesylwio <thesylwio@gmail.com>
2021-03-26 22:38:25 +0100

    ✨ Created ScreenOptions utility

---
+++ app/constants/ScreenOptions.ts
@@ -0,0 +1,13 @@
+import { StackNavigationOptions } from '@react-navigation/stack';
+
+const ScreenOptions: StackNavigationOptions = {
+  headerTitleStyle: { alignSelf: 'center' },
+  headerStyle: {
+    backgroundColor: '#F8FAFB',
+    elevation: 0,
+    shadowOpacity: 0,
+    borderBottomWidth: 0,
+  },
+};
+
+export default ScreenOptions;

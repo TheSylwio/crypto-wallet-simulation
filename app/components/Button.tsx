55834d601b8f65b731c930a0df49386bc8ff9140
thesylwio <thesylwio@gmail.com>
2021-05-15 10:33:51 +0200

    ✨ Added disabled property into Button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -7,18 +7,20 @@ interface Props {
   onPress: () => any;
   color: string;
   style?: object;
+  disabled?: boolean;
 }
 
-const Button: React.FC<Props> = ({ title, onPress, color, style }) => {
+const Button: React.FC<Props> = ({ title, onPress, color, style, disabled = false }) => {
   return (
-    <StyledButton style={style} onPress={onPress} color={color} activeOpacity={0.7}>
+    <StyledButton style={style} onPress={onPress} color={color} activeOpacity={0.7} disabled={disabled}>
       <ButtonText>{title}</ButtonText>
     </StyledButton>
   );
 };
 
-const StyledButton = styled.TouchableOpacity<{color: string}>`
+const StyledButton = styled.TouchableOpacity<{ color: string; disabled: boolean; }>`
   background: ${({ color }) => color};
+  opacity: ${({ disabled }) => disabled ? 0.65 : 1};
   elevation: 1;
   padding: 16px;
   flex: 1;
